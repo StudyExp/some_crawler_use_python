33955d2e3aa8c605f59c742bd6489f3048f5d329
Cyrill <lichunyang@outlook.com>
2017-08-03 18:14:07 +0800

    优化代码

--- python_ crawler/dbmeinv_img.py
+++ python_ crawler/dbmeinv_img.py
@@ -1,18 +1,16 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-#encoding=utf-8
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import urllib2
 import urllib
-import os
 from bs4 import BeautifulSoup
+
+
 def getAllImageLink(index):
 
     str = 'http://www.dbmeinv.com/?pager_offset=%s' % index
 
     html = urllib2.urlopen(str).read()
-
-    # html = urllib2.urlopen('http://www.ivsky.com/tupian/mujinhua_v37031/').read()
 
     soup = BeautifulSoup(html, "lxml")
 
@@ -22,22 +20,20 @@ def getAllImageLink(index):
 
     print liResult
 
-    i=0
+    i = 0
 
     for li in liResult:
-        i = i+1
+        i = i + 1
         imageEntityArray = li.findAll('img')
         for image in imageEntityArray:
             link = image.get('src')
-            imageName = image.get('title')
+            # imageName = image.get('title')
             # filesavepath = '/Users/cyrill/Desktop/DBMeiNv/%s.jpg' % imageName
-            filesavepath = '/Users/cyrill/Desktop/DBMeiNv/%s-%s.jpg' %(index, i)
+            filesavepath = '/Users/cyrill/Desktop/DBMeiNv/%s-%s.jpg' % (index, i)
             urllib.urlretrieve(link, filesavepath)
             print filesavepath
+
 
 if __name__ == '__main__':
     for index in range(1, 10, 1):
         getAllImageLink(index)
-
-
-
--- python_ crawler/jiandan_img.py
+++ python_ crawler/jiandan_img.py
@@ -1,14 +1,14 @@
+# !/usr/bin/env python3
 # coding:utf-8
 
 import urllib.request
-import os
-import re
+import os, sys, re
 
 
 def url_open(url):
 
     if not ('http' in url):
-        url = 'http://'+url
+        url = 'http://' + url
     print('url is :' + url)
     req = urllib.request.Request(url)
     req.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:36.0) Gecko/20100101 Firefox/36.0')
@@ -17,16 +17,22 @@ def url_open(url):
 
 
 def get_page(url):
+
     html = url_open(url).decode('utf-8')
-    pattern = r'<span class="current-comment-page">\[(\d{4})\]</span>'  #正则表达式寻找页面地址
+    # html 是一个字符串
 
-    print(pattern)
+    pattern = r'<span class="current-comment-page">\[(\d*)\]</span>'
+    # 正则表达式寻找页面地址
 
-    bbb = re.findall(pattern, html)
+    page = 0
+    pagelist = re.findall(pattern, html)
 
-    print(bbb)
-    # page = int(re.findall(pattern,html)[0])
-    page = 150
+    try:
+        page = int(pagelist[0])
+    except:
+        page = 0
+
+    print(page)
     return page
 
 
@@ -40,62 +46,54 @@ def find_imgs(page_url):
 def save_imgs(img_addrs, page_num, folder):
     if not os.path.exists(str(page_num)):
         os.mkdir(str(page_num))
+
     os.chdir(str(page_num))
+
     for i in img_addrs:
         pattern = r'sinaimg.cn/mw600/(.*?).jpg'
         print('pattern is :' + pattern)
         filename = i.split('/')[-1]
         print('filename is :' + filename)
-        print(i)
 
         url_str = i.split('//')[-1]
-        url = 'http://'+url_str
-        f = open(filename, 'wb')
+        url = 'http://' + url_str
+
+        with open("../images.txt", 'a+') as f:
+            f.writelines(url + '\n')
+            f.close()
 
         req = urllib.request.urlopen(url)
-        buf = req.read()
+        image = req.read()
 
-        f.write(buf)
-        f.close()
-
-        # image = url_open(urlStr)
-        # print(image)
-
-        # with open(filename,'wb') as fnot:
-        #     f.write(image)
-        #     f.close()
+        with open(filename, 'wb') as f:
+            f.write(image)
+            f.close()
 
 
-def download_mm(folder='abc', pages=1):
+def download_mm(folder='abc', pages=10):
     if not os.path.exists(folder):
         os.mkdir(folder)  # 新建文件夹
         print('成功创建文件夹', folder)
 
-    os.chdir(folder)  #跳转到文件夹
-    folder_top = os.getcwd()  #获取当前工作目录
+    os.chdir(folder)  # 跳转到文件夹
+    folder_top = os.getcwd()  # 获取当前工作目录
     url = 'http://jandan.net/ooxx/'
-    page_num = get_page(url)  #获取网页最新的地址
-    print(page_num)
+    page_nums = get_page(url)  # 获取网页最新的地址
+    print(page_nums)
     for i in range(pages):
-        page_num -= i  #递减下载几个网页
-        page_url = url + 'page-' + str(page_num) + '#comments'  #组合网页地址
-        img_addrs = find_imgs(page_url)  #获取图片地址
+        page_num = page_nums - i  # 递减下载几个网页
+        page_url = url + 'page-' + str(page_num) + '#comments'  # 组合网页地址
+        img_addrs = find_imgs(page_url)  # 获取图片地址
 
         print(img_addrs)
         print(folder)
         print(pages)
-        save_imgs(img_addrs, page_num, folder)  #保存图片
+        save_imgs(img_addrs, page_num, folder)  # 保存图片
         os.chdir(folder_top)
 
 
 if __name__ == '__main__':
     folder = input("Please enter a folder(default is 'abc'): ")
     pages = input("How many pages do you wan to download(default is 10): ")
-
-    # a = str(folder)
-    # b = int(pages)
-    #
-    # print(a)
-    # print(b)
 
     download_mm()
